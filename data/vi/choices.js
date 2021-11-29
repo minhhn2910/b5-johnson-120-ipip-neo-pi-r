1f4536435acd2d1eae0738448d2fb383fd38626d
Minh <minhhn2910@gmail.com>
2021-11-29 14:40:06 +0800

    vietnamese trans first commit

---
+++ data/vi/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'Hoàn toàn không đồng ý',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Không đồng ý',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Sao cũng được',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Đồng ý ',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Hoàn toàn đồng ý',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Hoàn toàn không đồng ý',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Không đồng ý',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Sao cũng được',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Đồng ý',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Hoàn toàn đồng ý',
+      score: 1,
+      color: 5
+    }
+  ]
+}
